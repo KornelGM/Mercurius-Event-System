7523b6c9c5941cf1935126ac5b6c12f18708de8a
KornelGM <KORNik_k@wp.pl>
2020-10-02 20:01:18 +0200

    Added shared variable support

---
+++ Shared Variables/SharedFloatVariable.cs
@@ -0,0 +1,9 @@
+﻿/**
+ * Description: An example implementation of a float Shared Variable.
+ * Copyright: © 2017-2020 Kornel, MIT License. Please see 'README.md' and 'LICENSE' files for more information.
+ **/
+
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Float Shared Variable", menuName = "Shared Variable/Float Variable")]
+public class SharedFloatVariable: SharedVariableBase<float> { }
---
+++ SharedVariableBase.cs
@@ -0,0 +1,22 @@
+﻿/**
+ * Description: Shared variable using Scriptable Object.
+ * Inspired by: https://unity.com/how-to/architect-game-code-scriptable-objects
+ * Copyright: © 2017-2020 Kornel, MIT License. Please see 'README.md' and 'LICENSE' files for more information.
+ **/
+
+using System;
+using UnityEngine;
+
+public class SharedVariableBase<T>: ScriptableObject, ISerializationCallbackReceiver
+{
+	[SerializeField] private T _startValue = default;
+
+	[NonSerialized] public T Value;
+
+	public void OnAfterDeserialize()
+	{
+		Value = _startValue;
+	}
+
+	public void OnBeforeSerialize() { }
+}
