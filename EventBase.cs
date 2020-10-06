e17d77e003e54e3fd511f9cfec4f2a35858595ee
KornelGM <KORNik_k@wp.pl>
2020-10-06 08:52:04 +0200

    Added the ability to subscribe/unsubscribe using the event (ScriptableObject) itself, skipping event listener script

--- EventBase.cs
+++ EventBase.cs
@@ -3,12 +3,14 @@
  * Copyright: © 2017-2020 Kornel, MIT License. Please see 'README.md' and 'LICENSE' files for more information.
  **/
 
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EventBase: ScriptableObject, IEvent
 {
 	private List<IEventListener> listeners = new List<IEventListener>();
+	private List<Action> listenersA = new List<Action>();
 
 	public void Broadcast(GameObject gameObject)
 	{
@@ -25,6 +27,11 @@ public class EventBase: ScriptableObject, IEvent
 		{
 			listeners[i].OnEventRaised();
 		}
+
+		for(int i = listenersA.Count - 1; i >= 0; i--)
+		{
+			listenersA[i].Invoke();
+		}
 	}
 
 	public void Subscribe(IEventListener listener)
@@ -32,15 +39,26 @@ public class EventBase: ScriptableObject, IEvent
 		listeners.Add(listener);
 	}
 
+	public void Subscribe(Action listener)
+	{
+		listenersA.Add(listener);
+	}
+
 	public void UnSubscribe(IEventListener listener)
 	{
 		listeners.Remove(listener);
+	}
+
+	public void UnSubscribe(Action listener)
+	{
+		listenersA.Remove(listener);
 	}
 }
 
 public class EventBase<T>: ScriptableObject, IEvent<T>
 {
 	private List<IEventListener<T>> listeners = new List<IEventListener<T>>();
+	private List<Action<T>> listenersA = new List<Action<T>>();
 
 	public void Broadcast(GameObject gameObject, T parameter)
 	{
@@ -57,6 +75,11 @@ public class EventBase<T>: ScriptableObject, IEvent<T>
 		{
 			listeners[i].OnEventRaised(parameter);
 		}
+
+		for(int i = listenersA.Count - 1; i >= 0; i--)
+		{
+			listenersA[i].Invoke(parameter);
+		}
 	}
 
 	public void Subscribe(IEventListener<T> listener)
@@ -64,15 +87,26 @@ public class EventBase<T>: ScriptableObject, IEvent<T>
 		listeners.Add(listener);
 	}
 
+	public void Subscribe(Action<T> listener)
+	{
+		listenersA.Add(listener);
+	}
+
 	public void UnSubscribe(IEventListener<T> listener)
 	{
 		listeners.Remove(listener);
+	}
+
+	public void UnSubscribe(Action<T> listener)
+	{
+		listenersA.Remove(listener);
 	}
 }
 
 public class EventBase<T, U>: ScriptableObject, IEvent<T, U>
 {
 	private List<IEventListener<T, U>> listeners = new List<IEventListener<T, U>>();
+	private List<Action<T, U>> listenersA = new List<Action<T, U>>();
 
 	public void Broadcast(GameObject gameObject, T parameter1, U parameter2)
 	{
@@ -89,6 +123,11 @@ public class EventBase<T, U>: ScriptableObject, IEvent<T, U>
 		{
 			listeners[i].OnEventRaised(parameter1, parameter2);
 		}
+
+		for(int i = listenersA.Count - 1; i >= 0; i--)
+		{
+			listenersA[i].Invoke(parameter1, parameter2);
+		}
 	}
 
 	public void Subscribe(IEventListener<T, U> listener)
@@ -96,8 +135,18 @@ public class EventBase<T, U>: ScriptableObject, IEvent<T, U>
 		listeners.Add(listener);
 	}
 
+	public void Subscribe(Action<T, U> listener)
+	{
+		listenersA.Add(listener);
+	}
+
 	public void UnSubscribe(IEventListener<T, U> listener)
 	{
 		listeners.Remove(listener);
+	}
+
+	public void UnSubscribe(Action<T, U> listener)
+	{
+		listenersA.Remove(listener);
 	}
 }
